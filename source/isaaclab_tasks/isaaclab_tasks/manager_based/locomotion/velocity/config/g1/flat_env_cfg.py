73cd55d97781a6b0a41f9d9d19a6a3dfa11093ac
robotics <qkrtjdrhks21@gmail.com>
2025-04-03 11:26:09 +0900

    250403_2

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/flat_env_cfg.py
@@ -26,7 +26,7 @@ class G1FlatEnvCfg(G1RoughEnvCfg):
         # New Rewards
         self.rewards.joint_deviation_arms.weight = -0.2
         self.rewards.joint_deviation_fingers.weight = -0.1
-        self.rewards.joint_deviation_torso.weight = -0.6
+        self.rewards.joint_deviation_torso.weight = -0.4
         # Rewards
         self.rewards.track_ang_vel_z_exp.weight = 1.0
         self.rewards.lin_vel_z_l2.weight = -0.2
