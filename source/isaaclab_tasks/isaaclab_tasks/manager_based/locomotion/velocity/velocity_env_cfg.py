743fd3fe25b7455ad45231b50fb3e693b8582ebb
robotics <qkrtjdrhks21@gmail.com>
2025-08-18 12:11:51 +0900

    1211

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -741,11 +741,11 @@ class EventCfg:
         },
     )
 
-    randomize_base_com = EventTerm(
-        func=mdp.randomize_rigid_body_com,
+    randomize_object_com = EventTerm(
+        func=mdp.randomize_object_com,
         mode="startup",
         params={
-            "asset_cfg": SceneEntityCfg("object",body_names=".*"),
+            "asset_cfg": SceneEntityCfg("object"),
             "com_range": {"x": (-0.1, 0.1), "y": (-0.1, 0.1), "z": (-0.1, 0.1)},
         },
     )
