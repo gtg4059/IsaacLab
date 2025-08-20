5f86e9162c2e544452d842c2f661c3112e88edd6
robotics <qkrtjdrhks21@gmail.com>
2025-08-20 23:52:13 +0900

    friction_fix

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -104,35 +104,35 @@ class CommandsCfg:
         ),
     )
 
-    left_ee_pose = mdp.UniformPoseCommandCfg(
-        asset_name="robot",
-        body_name="L_middle_proximal",
-        resampling_time_range=(30.0, 30.0),
-        debug_vis=True,
-        ranges=mdp.UniformPoseCommandCfg.Ranges(
-            pos_x=(0.38, 0.38),
-            pos_y=(0.2, 0.2),
-            pos_z=(0.10, 0.10),
-            roll=(-0.0, 0.0),
-            pitch=(-0.0, 0.0),
-            yaw=(math.pi / 2.0, math.pi / 2.0),#(-math.pi / 2.0 - 0.1, -math.pi / 2.0 + 0.1),
-        ),
-    )
+    # left_ee_pose = mdp.UniformPoseCommandCfg(
+    #     asset_name="robot",
+    #     body_name="L_middle_proximal",
+    #     resampling_time_range=(30.0, 30.0),
+    #     debug_vis=True,
+    #     ranges=mdp.UniformPoseCommandCfg.Ranges(
+    #         pos_x=(0.38, 0.38),
+    #         pos_y=(0.2, 0.2),
+    #         pos_z=(0.10, 0.10),
+    #         roll=(-0.0, 0.0),
+    #         pitch=(-0.0, 0.0),
+    #         yaw=(math.pi / 2.0, math.pi / 2.0),#(-math.pi / 2.0 - 0.1, -math.pi / 2.0 + 0.1),
+    #     ),
+    # )
 
-    right_ee_pose = mdp.UniformPoseCommandCfg(
-        asset_name="robot",
-        body_name="R_middle_proximal",
-        resampling_time_range=(30.0, 30.0),
-        debug_vis=True,
-        ranges=mdp.UniformPoseCommandCfg.Ranges(
-            pos_x=(0.38, 0.38),
-            pos_y=(-0.2, -0.2),
-            pos_z=(0.10, 0.10),
-            roll=(-0.0, 0.0),
-            pitch=(-0.0, 0.0),
-            yaw=(-math.pi / 2.0, -math.pi / 2.0),#(-math.pi / 2.0 - 0.1, -math.pi / 2.0 + 0.1),
-        ),
-    )
+    # right_ee_pose = mdp.UniformPoseCommandCfg(
+    #     asset_name="robot",
+    #     body_name="R_middle_proximal",
+    #     resampling_time_range=(30.0, 30.0),
+    #     debug_vis=True,
+    #     ranges=mdp.UniformPoseCommandCfg.Ranges(
+    #         pos_x=(0.38, 0.38),
+    #         pos_y=(-0.2, -0.2),
+    #         pos_z=(0.10, 0.10),
+    #         roll=(-0.0, 0.0),
+    #         pitch=(-0.0, 0.0),
+    #         yaw=(-math.pi / 2.0, -math.pi / 2.0),#(-math.pi / 2.0 - 0.1, -math.pi / 2.0 + 0.1),
+    #     ),
+    # )
 
 
 @configclass
@@ -269,14 +269,14 @@ class ObservationsCfg:
         actions = ObsTerm(func=mdp.last_action)
         #########################################################################################
         velocity_commands = ObsTerm(func=mdp.generated_commands, params={"command_name": "base_velocity"},scale=(2.0,2.0,0.25))# 3
-        left_ee_pose_command = ObsTerm(
-            func=mdp.generated_commands,
-            params={"command_name": "left_ee_pose"},
-        )
-        right_ee_pose_command = ObsTerm(
-            func=mdp.generated_commands,
-            params={"command_name": "right_ee_pose"},
-        )
+        # left_ee_pose_command = ObsTerm(
+        #     func=mdp.generated_commands,
+        #     params={"command_name": "left_ee_pose"},
+        # )
+        # right_ee_pose_command = ObsTerm(
+        #     func=mdp.generated_commands,
+        #     params={"command_name": "right_ee_pose"},
+        # )
 
         def __post_init__(self):
             self.enable_corruption = True
@@ -368,14 +368,14 @@ class ObservationsCfg:
         actions = ObsTerm(func=mdp.last_action)
         #########################################################################################
         velocity_commands = ObsTerm(func=mdp.generated_commands, params={"command_name": "base_velocity"},scale=(2.0,2.0,0.25))# 3
-        left_ee_pose_command = ObsTerm(
-            func=mdp.generated_commands,
-            params={"command_name": "left_ee_pose"},
-        )
-        right_ee_pose_command = ObsTerm(
-            func=mdp.generated_commands,
-            params={"command_name": "right_ee_pose"},
-        )
+        # left_ee_pose_command = ObsTerm(
+        #     func=mdp.generated_commands,
+        #     params={"command_name": "left_ee_pose"},
+        # )
+        # right_ee_pose_command = ObsTerm(
+        #     func=mdp.generated_commands,
+        #     params={"command_name": "right_ee_pose"},
+        # )
 
     # observation groups
     policy: PolicyCfg = PolicyCfg()
@@ -549,8 +549,8 @@ class EventCfg:
         mode="reset",
         params={
             "asset_cfg": SceneEntityCfg("robot", joint_names=".*"),
-            "friction_distribution_params": (0.01, 1.15),
-            "viscous_friction_distribution_params": (0.3, 1.5),
+            "friction_distribution_params": (0.01, 1.0),
+            "viscous_friction_distribution_params": (0.01, 1.0),
             "armature_distribution_params": (0.008,0.06),
             "operation": "add",
             "distribution": "uniform",
