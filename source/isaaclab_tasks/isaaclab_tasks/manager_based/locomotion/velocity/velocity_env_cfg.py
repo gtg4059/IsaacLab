7d02b1c06e33070d6dfdc4fcb9cdde1450cd8bde
gtg4059 <qkrtjdrhks21@gmail.com>
2025-08-30 22:16:10 +0900

    250830

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -452,7 +452,7 @@ class EventCfg:
         interval_range_s=(2.0,3.0),
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names=['torso_link']),
-            "com_range": {"x": (-0.00, 0.5), "y": (-0.2, 0.2), "z": (-0.2, 0.2)},
+            "com_range": {"x": (-0.00, 0.2), "y": (-0.08, 0.08), "z": (-0.08, 0.08)},
         },
     )
 
