d7507fd662e77cbcfc6b2c760bc097443419edd1
gtg4059 <qkrtjdrhks21@gmail.com>
2025-09-05 11:16:27 +0900

    hand_mass_dist

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -687,6 +687,17 @@ class EventCfg:
         },
     )
 
+    randomize_hand_mass = EventTerm(
+        func=mdp.randomize_rigid_body_mass,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names=["left_wrist_yaw_link", 
+                                                             "right_wrist_yaw_link"]),
+            "mass_distribution_params": (0.1, 0.5),
+            "operation": "add",
+        },
+    )
+
     randomize_base_com = EventTerm(
         func=mdp.randomize_rigid_body_com,
         mode="startup",
