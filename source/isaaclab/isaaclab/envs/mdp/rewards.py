b1cf4c1d5aeee45a1444ccff9d44367e7ab028ab
gtg4059 <qkrtjdrhks21@gmail.com>
2025-09-08 16:04:15 +0900

    delete_table

--- source/isaaclab/isaaclab/envs/mdp/rewards.py
+++ source/isaaclab/isaaclab/envs/mdp/rewards.py
@@ -385,7 +385,7 @@ def delete_table(
     """
     asset: RigidObject = env.scene[asset_cfg.name]
     # print(env.common_step_counter)
-    if env.common_step_counter > 2400:
+    if env.common_step_counter > env.max_episode_length*1000:
         asset.data.root_pos_w[:, 2] -= 0.001*torch.ones_like(asset.data.root_pos_w[:, 2],device=asset.device)
         asset.write_root_state_to_sim(asset.data.root_state_w)
         asset.write_data_to_sim()
