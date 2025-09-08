6d772ff7184eab3056a678c2829defd2f8209f54
gtg4059 <qkrtjdrhks21@gmail.com>
2025-09-08 14:59:21 +0900

    1459

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/rough_env_cfg.py
@@ -64,7 +64,7 @@ class G1Rewards(RewardsCfg):
         weight=1.2
     )
     object_is_lifted = RewTerm(func=mdp.object_is_lifted, 
-                               weight=0.5,
+                               weight=0.8,
                                params={"std": 0.3,
                                        "minimal_height": 0.87,
                                        "height": 0.92,
