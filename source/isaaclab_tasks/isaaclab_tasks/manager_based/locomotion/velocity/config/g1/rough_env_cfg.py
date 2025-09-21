8e5d961ebbe0ca140243ae8875c22bd0a61ae83d
robotics <qkrtjdrhks21@gmail.com>
2025-09-21 23:12:51 +0900

    ys0921_3

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/rough_env_cfg.py
@@ -67,7 +67,7 @@ class G1Rewards(RewardsCfg):
 
     track_lin_vel_xy_exp = RewTerm(
         func=mdp.track_lin_vel_xy_yaw_frame_exp,
-        weight=1.8,#3.0
+        weight=1.5,#3.0
         params={"command_name": "base_velocity", "std": 0.5},# std = 0.5
     )
     track_ang_vel_z_exp = RewTerm(
@@ -171,7 +171,7 @@ class G1Rewards(RewardsCfg):
 
     joint_deviation_arms = RewTerm(
         func=mdp.joint_deviation_l1,
-        weight=-0.1,#-0.1
+        weight=-0.2,#-0.1
         params={
             "asset_cfg": SceneEntityCfg(
                 "robot",
@@ -388,7 +388,7 @@ class G1RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.dof_torques_l2.params["asset_cfg"] = SceneEntityCfg(
             "robot", joint_names=[".*_hip_.*", ".*_knee_joint", ".*_ankle_.*"]
         )
-        self.rewards.action_rate_arm.weight = -0.001
+        self.rewards.action_rate_arm.weight = -0.005
 
 
 @configclass
