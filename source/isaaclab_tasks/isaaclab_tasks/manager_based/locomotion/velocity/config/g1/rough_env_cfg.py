284df7967c80d1b171dacea0ca865ffe2ec47149
gtg4059 <qkrtjdrhks21@gmail.com>
2025-09-27 07:07:32 +0900

    joint_deviation_shoulders

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/rough_env_cfg.py
@@ -64,7 +64,7 @@ class G1Rewards(RewardsCfg):
         weight=-1.0,
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*_hip_yaw_joint", ".*_hip_roll_joint"])},
     )
-    
+
     joint_deviation_arms = RewTerm(
         func=mdp.joint_deviation_l1,
         weight=-0.1,
@@ -79,6 +79,25 @@ class G1Rewards(RewardsCfg):
                     ".*_wrist_roll_joint",
                     ".*_wrist_pitch_joint",
                     ".*_wrist_yaw_joint",
+                ],
+            )
+        },
+    )
+    
+    joint_deviation_shoulders = RewTerm(
+        func=mdp.joint_deviation_l1,
+        weight=-0.2,
+        params={
+            "asset_cfg": SceneEntityCfg(
+                "robot",
+                joint_names=[
+                    ".*_shoulder_pitch_joint",
+                    # ".*_shoulder_roll_joint",
+                    # ".*_shoulder_yaw_joint",
+                    # ".*_elbow_joint",
+                    # ".*_wrist_roll_joint",
+                    # ".*_wrist_pitch_joint",
+                    # ".*_wrist_yaw_joint",
                 ],
             )
         },
