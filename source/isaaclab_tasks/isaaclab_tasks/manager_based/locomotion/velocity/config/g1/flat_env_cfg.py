9f08db443301ecc44297ce8a25dbb31dd35e74d6
gtg4059 <qkrtjdrhks21@gmail.com>
2025-10-16 15:30:35 +0900

    command_fix_4

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/flat_env_cfg.py
@@ -48,8 +48,8 @@ class G1FlatEnvCfg(G1RoughEnvCfg):
         self.rewards.joint_deviation_torso.weight = -1.0
         
         # Commands
-        self.commands.base_velocity.ranges.lin_vel_x = (-1.0, 1.0)
-        self.commands.base_velocity.ranges.lin_vel_y = (-1.0, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_x = (-10.0, 10.0)
+        self.commands.base_velocity.ranges.lin_vel_y = (-10.0, 10.0)
         self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
         self.commands.base_velocity.ranges.heading = (-3.14, 3.14)
         # self.commands.base_velocity.ranges.lin_vel_x = (-0.0, 0.0)
