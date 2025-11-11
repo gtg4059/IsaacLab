239b0f6c4f7206c7d650ad19a65c0d73bc5b9d42
gtg4059 <qkrtjdrhks21@gmail.com>
2025-11-12 00:10:34 +0900

    251112

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -551,7 +551,7 @@ class RewardsCfg:
     ang_vel_xy_l2 = RewTerm(func=mdp.ang_vel_xy_l2, weight=-0.05)
     dof_torques_l2 = RewTerm(func=mdp.joint_torques_l2, weight=-5.0e-5)
     dof_acc_l2 = RewTerm(func=mdp.joint_acc_l2, weight=-1.0e-6)
-    action_rate_l2 = RewTerm(func=mdp.action_rate_l2, weight=-0.01)
+    action_rate_l2 = RewTerm(func=mdp.action_rate_l2, weight=-0.02)
     # action_rate_arm = RewTerm(func=mdp.action_rate_arm, weight=-0.1)
     undesired_contacts = RewTerm(
         func=mdp.undesired_contacts,
