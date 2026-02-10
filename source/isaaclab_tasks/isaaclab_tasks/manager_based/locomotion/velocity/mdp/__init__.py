634f7357c45ded3e62deea7035ba09f072f5b93f
gtg4059 <qkrtjdrhks21@gmail.com>
2026-02-10 13:43:09 +0900

    start

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/mdp/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/mdp/__init__.py
@@ -9,5 +9,6 @@ from isaaclab.envs.mdp import *  # noqa: F401, F403
 
 from .curriculums import *  # noqa: F401, F403
 from .dual_pose_command_cfg import *  # noqa: F401, F403
+from .events import *  # noqa: F401, F403
 from .rewards import *  # noqa: F401, F403
 from .terminations import *  # noqa: F401, F403
