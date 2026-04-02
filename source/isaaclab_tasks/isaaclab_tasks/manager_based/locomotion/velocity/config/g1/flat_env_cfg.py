c8f234227f3ccca1ae003b00e71a4a711a53663d
gtg4059 <qkrtjdrhks21@gmail.com>
2026-04-02 14:57:58 +0900

    base_height_0.77

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/flat_env_cfg.py
@@ -78,7 +78,7 @@ class G1FlatEnvCfg_PLAY(G1FlatEnvCfg):
         self.scene.num_envs = 50
         self.scene.env_spacing = 2.5
         self.curriculum.arm_joint_targets_position_range = None
-        self.events.set_arm_joint_targets_interval.params["position_range"] = (-0.8, 0.8)
+        # self.events.set_arm_joint_targets_interval.params["position_range"] = (-0.8, 0.8)
         # self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.0)
         # self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
         # self.commands.base_velocity.ranges.ang_vel_z = (0.0, 0.0)
