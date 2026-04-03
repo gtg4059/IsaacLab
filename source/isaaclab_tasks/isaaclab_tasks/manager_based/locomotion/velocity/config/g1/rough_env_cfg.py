275882d28e668d79efb147123fd7a0a8630b632d
gtg4059 <qkrtjdrhks21@gmail.com>
2026-04-03 14:39:47 +0900

    260403_1

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/g1/rough_env_cfg.py
@@ -16,7 +16,7 @@ from isaaclab_tasks.manager_based.locomotion.velocity.velocity_env_cfg import Lo
 ##
 from isaaclab_assets import G1_DEX_FIX, G1_DEX_EASY  # isort: skip
 STEP = 32000*8
-RESUME = 0*8
+RESUME = 64000*8
 
 @configclass
 class G1RoughRewards(RewardsCfg):
@@ -181,38 +181,38 @@ class G1RoughCurriculumCfg(CurriculumCfg):
         params={
             "step_interval": 1,#0.25*STEP,
             "percent_per_interval": 0.5,
-            "min_steps": 0.25*STEP-RESUME,
+            "min_steps": STEP-RESUME,
         },
     ) 
-    # foot_clearance_weight = CurrTerm(
-    #     func=mdp.modify_reward_weight,
-    #     params={
-    #         "term_name": "foot_clearance", "weight": 0.0, "num_steps": STEP-RESUME
-    #     }
-    # )
+    foot_clearance_weight = CurrTerm(
+        func=mdp.modify_reward_weight,
+        params={
+            "term_name": "foot_clearance", "weight": 0.0, "num_steps": 2*STEP-RESUME
+        }
+    )
 
     # 초기 50.0에서 num_steps 동안 선형 감쇠하여 0으로 수렴
     # min_steps 이전에는 weight=0, 이후 initial_weight에서 final_weight로 num_steps 동안 감쇠
-    # feet_land_time_weight = CurrTerm(
-    #     func=mdp.modify_reward_weight_linear_decay,
-    #     params={
-    #         "term_name": "feet_land_time",
-    #         "initial_weight": 80.0,
-    #         "final_weight": 2.0,
-    #         "num_steps": 2.1*STEP-RESUME,
-    #         "min_steps": 2*STEP-RESUME,  # 이 스텝 이후부터 감쇠 시작 (이전에는 weight=0)
-    #     },
-    # )
+    feet_land_time_weight = CurrTerm(
+        func=mdp.modify_reward_weight_linear_decay,
+        params={
+            "term_name": "feet_land_time",
+            "initial_weight": 80.0,
+            "final_weight": 2.0,
+            "num_steps": 2.6*STEP-RESUME,
+            "min_steps": 2.5*STEP-RESUME,  # 이 스텝 이후부터 감쇠 시작 (이전에는 weight=0)
+        },
+    )
 
-    # contact_forces_weight = CurrTerm(
-    #     func=mdp.modify_reward_weight,
-    #     params={"term_name": "contact_forces", "weight": -0.0000005, "num_steps": STEP-RESUME}
-    # )
+    contact_forces_weight = CurrTerm(
+        func=mdp.modify_reward_weight,
+        params={"term_name": "contact_forces", "weight": -0.0000005, "num_steps": 2*STEP-RESUME}
+    )
 
-    # action_rate_l2_weight = CurrTerm(
-    #     func=mdp.modify_reward_weight,
-    #     params={"term_name": "action_rate_l2", "weight": -0.01, "num_steps": STEP-RESUME}
-    # )
+    action_rate_l2_weight = CurrTerm(
+        func=mdp.modify_reward_weight,
+        params={"term_name": "action_rate_l2", "weight": -0.002, "num_steps": 2*STEP-RESUME}
+    )
 
 @configclass
 class G1RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
@@ -239,7 +239,7 @@ class G1RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.commands.base_velocity.ranges.lin_vel_y = (-0.02, 0.02)
 
         self.rewards.undesired_contacts = None
-        self.rewards.base_height.params["target_height"] = 0.75
+        self.rewards.base_height.params["target_height"] = 0.76
         self.rewards.foot_clearance.weight = 0.75
         self.rewards.feet_land_time.weight = 0.0
         self.rewards.contact_forces.weight = 0.0
